cbb7b8098143f3472e12c874bb70013563f22c32
hexzoner <hexzon@gmail.com>
2024-10-15 20:14:53 +0200

    small fix

--- src/pages/user/Sales.tsx
+++ src/pages/user/Sales.tsx
@@ -1,3 +1,3 @@
 export default function Sales() {
-  return <div className="min-h-screen flex flex-col justify-center items-center text-2xl">Sales - to be added</div>;
+  return <div className="min-h-screen flex flex-col justify-center items-center text-2xl">Sales - to be added...</div>;
 }
