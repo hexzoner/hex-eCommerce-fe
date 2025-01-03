23f62825981256ed7996f167df6127904b7b0ae2
hexzoner <hexzon@gmail.com>
2025-01-03 19:25:13 +0100

    homepage redesign

--- src/pages/user/home-components/ReviewsCarousel.tsx
+++ src/pages/user/home-components/ReviewsCarousel.tsx
@@ -25,18 +25,16 @@ function ReviewsCarousel({ reviews }: { reviews: Review[] }) {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
+          slidesToShow: 2,
+          slidesToScroll: 2,
         },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
         },
       },
       {
